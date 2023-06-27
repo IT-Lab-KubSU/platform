a9f5e760fdc28f5b88a7e9030ed9db5259f362a8
Artem-Darius Atlas <mit.3tlasa@gmail.com>
2023-06-27 03:39:07 +0300

    feat: added graphql for AGW module with auth

--- apps/agw/src/app/app.module.ts
+++ apps/agw/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ApiGatewayModule} from "../api-gateway/api-gateway.module";
 
 @Module({
-  imports: [],
+  imports: [ApiGatewayModule],
   controllers: [AppController],
   providers: [AppService],
 })
