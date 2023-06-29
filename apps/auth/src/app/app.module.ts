f5d32f6789e18c659c28d7addf1d452fb78713a0
Artem-Darius Atlas <mit.3tlasa@gmail.com>
2023-06-29 16:11:55 +0300

    feat: added submodules in the app.module.ts for the auth service

--- apps/auth/src/app/app.module.ts
+++ apps/auth/src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import PortConfig from "../config/port.config";
+import KeysConfig from "../config/keys.config";
+import { UserModule } from "../user/user.module";
+import { DeviceModule } from "../device/device.module";
+import { TokenModule } from "../token/token.module";
+import { PrismaModule } from "../prisma/prisma.module";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [PortConfig, KeysConfig] }),
+    UserModule,
+    DeviceModule,
+    TokenModule,
+    PrismaModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
